169a6ab2cf29a89d7e3024ceadb0df4239976cbe
TuismoreAI <brumanswaves@gmail.com>
2026-07-13 18:21:11 +0200

    Wrap report with local property team

---
+++ src/components/property/ErfResearchDossierWithLocalTeam.tsx
@@ -0,0 +1,27 @@
+import type { NormalizedOfficialParcel } from "@/lib/parcels/officialParcelId";
+import {
+  ErfResearchDossier as BaseErfResearchDossier,
+  type DossierView,
+} from "@/components/property/ErfResearchDossier.tsx";
+import { LocalPropertyTeam } from "./dossier/LocalPropertyTeam";
+
+interface Props {
+  parcel: NormalizedOfficialParcel;
+  view?: DossierView;
+  onSelectView?: (view: DossierView) => void;
+}
+
+export type { DossierView };
+
+export function ErfResearchDossier(props: Props) {
+  if (props.view !== "stoep-report") {
+    return <BaseErfResearchDossier {...props} />;
+  }
+
+  return (
+    <div className="space-y-5">
+      <BaseErfResearchDossier {...props} />
+      <LocalPropertyTeam parcel={props.parcel} />
+    </div>
+  );
+}
